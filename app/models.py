0f370267a0aa023bd016cb23a2cb4f082874ad8d
diego-curiel <177177256+diego-curiel@users.noreply.github.com>
2025-05-01 20:13:40 -0400

    create Task model

--- app/models.py
+++ app/models.py
@@ -1,2 +1,19 @@
-from sqlmodel import SQLModel
+from datetime import datetime
+from typing import Optional
+
 from pydantic import BaseModel
+from sqlmodel import Field, SQLModel
+
+# Database model for tasks
+class Task(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    # Task title
+    title: str
+    # Optional task description
+    description: Optional[str]
+    # Is the task completed
+    completed: bool = Field(default=False)
+    # Creation date
+    created_at: datetime = Field(default_factory=datetime.utcnow)
+
+

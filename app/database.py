d8fdb62ebe6081f9c2a57f3ade267409fd126330
diego-curiel <177177256+diego-curiel@users.noreply.github.com>
2025-05-02 17:03:37 -0400

    feat: add function 'create_all_tables'

--- app/database.py
+++ app/database.py
@@ -1,3 +1,12 @@
 from sqlmodel import SQLModel, create_engine
+import models # Import the tables in order to fill the tables metadata
+
 
 engine = create_engine("sqlite:///../database.db")
+
+def create_all_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+if __name__ == "__main__":
+    create_all_tables()

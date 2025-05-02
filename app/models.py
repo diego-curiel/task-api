afe8d2b7675523824a44cc07d9bc8380f1a8fbef
diego-curiel <177177256+diego-curiel@users.noreply.github.com>
2025-05-02 17:13:25 -0400

    fix: change the default value of 'created_at' and 'description'

--- app/models.py
+++ app/models.py
@@ -1,8 +1,16 @@
-from datetime import datetime
+from datetime import datetime, timezone
 from typing import Optional
 
 from pydantic import BaseModel
 from sqlmodel import Field, SQLModel
+
+
+def datetime_now()-> datetime:
+    """
+    Returns the actual date and time in UTC
+    """
+    return datetime.now(timezone.utc)
+
 
 # Database model for tasks
 class Task(SQLModel, table=True):
@@ -10,10 +18,10 @@ class Task(SQLModel, table=True):
     # Task title
     title: str
     # Optional task description
-    description: Optional[str]
+    description: Optional[str] = None
     # Is the task completed
     completed: bool = Field(default=False)
     # Creation date
-    created_at: datetime = Field(default_factory=datetime.utcnow)
+    created_at: datetime = Field(default_factory=datetime_now)
 
 

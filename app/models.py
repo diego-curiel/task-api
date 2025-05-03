aad47f9e5f6dcc584d989fd1ffdf93d149b254fd
diego-curiel <177177256+diego-curiel@users.noreply.github.com>
2025-05-02 23:58:44 -0400

    refactor: separate the Task model into multiple classes

--- app/models.py
+++ app/models.py
@@ -1,27 +1,48 @@
-from datetime import datetime, timezone
+from datetime import datetime
 from typing import Optional
 
-from pydantic import BaseModel
+import pydantic as pyd
 from sqlmodel import Field, SQLModel
 
-
-def datetime_now() -> datetime:
-    """
-    Returns the actual date and time in UTC
-    """
-    return datetime.now(timezone.utc)
+from utils import datetime_now
 
 
-# Database model for tasks
-class Task(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
+# Base task model
+class BaseTask(SQLModel):
     # Task title
-    title: str
+    title: str = Field(max_length=120)
     # Optional task description
-    description: Optional[str] = None
-    # Is the task completed
+    description: Optional[str] = Field(default=None, nullable=True, 
+                                       max_length=255)
+    # Whether the task was completed
     completed: bool = Field(default=False)
     # Creation date
     created_at: datetime = Field(default_factory=datetime_now)
 
+
+# Database model for tasks
+class Task(BaseTask, SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+
+
+# Model used to create and update (PUT) new tasks
+class TaskCreate(BaseTask):
+    pass # Just in case something changes in the future
+
+
+# Model used to show tasks to the users
+class TaskPublic(BaseTask):
+    id: int
+
+
+# Model used to handle task patches
+class TaskPatch(pyd.BaseModel):
+    # Make the title optional since this is a patch model
+    title: Optional[str] = pyd.Field(default=None,max_length=120)
+    # The task description was optional since the beginning
+    description: Optional[str] = pyd.Field(default=None, max_length=255)
+    # This was also optional since the default value was False
+    completed: bool = pyd.Field(default=False)
+    # The creation date must default to None in order to not change anything
+    created_at: Optional[datetime] = pyd.Field(default=None)
 
